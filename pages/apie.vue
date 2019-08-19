d36d570ed4ae9b75be25bb8b8fe77e1df4c07a45
Mindas <mindaugas.lickus@webelevate.ie>
2019-08-19 13:11:56 +0100

    initial git 👍🎉

---
+++ pages/apie.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-layout>
+    <v-flex md5>
+      <v-card class="pa-3 ma-2 elevation-5">
+        <h2>Virginijus Mockus</h2>
+        <p>
+          Reiki istorija. Consectetur quis laboris excepteur dolore in eiusmod
+          dolor ut pariatur labore deserunt sit Lorem fugiat.
+        </p>
+        <p>
+          Labore adipisicing irure elit ipsum ipsum anim mollit est velit ea qui
+          reprehenderit. Anim commodo minim dolore magna reprehenderit. Aliquip
+          nostrud esse do pariatur labore sit in sit quis mollit non dolor sunt
+          magna. Est enim laborum occaecat reprehenderit laboris elit sunt
+          laborum proident labore consequat aliquip.
+        </p>
+        <h4 class="ma-5">Cia bus reiki medis</h4>
+        <h4 class="ma-5">Cia bus reiki medis</h4>
+        <h4 class="ma-5">Cia bus reiki medis</h4>
+        <h4 class="ma-5">Cia bus reiki medis</h4>
+        <h4 class="ma-5">Cia bus reiki medis</h4>
+        <h4 class="ma-5">Cia bus reiki medis</h4>
+        <h4 class="ma-5">Cia bus reiki medis</h4>
+
+        <v-img src="/virgis.jpg"></v-img>
+      </v-card>
+    </v-flex>
+    <v-flex md5 offset-md2>
+      <v-card class="pa-3 ma-2 elevation-5">
+        <h2>Mindaugas Lickus</h2>
+        <p>
+          Reiki istorija. Consectetur quis laboris excepteur dolore in eiusmod
+          dolor ut pariatur labore deserunt sit Lorem fugiat.
+        </p>
+        <p>
+          Labore adipisicing irure elit ipsum ipsum anim mollit est velit ea qui
+          reprehenderit. Anim commodo minim dolore magna reprehenderit. Aliquip
+          nostrud esse do pariatur labore sit in sit quis mollit non dolor sunt
+          magna. Est enim laborum occaecat reprehenderit laboris elit sunt
+          laborum proident labore consequat aliquip.
+        </p>
+        <h4 class="ma-5">Cia bus reiki medis</h4>
+        <h4 class="ma-5">Cia bus reiki medis</h4>
+        <h4 class="ma-5">Cia bus reiki medis</h4>
+        <h4 class="ma-5">Cia bus reiki medis</h4>
+        <h4 class="ma-5">Cia bus reiki medis</h4>
+        <h4 class="ma-5">Cia bus reiki medis</h4>
+        <h4 class="ma-5">Cia bus reiki medis</h4>
+
+        <v-img src="/mindaugas.jpg"></v-img>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <v-card>
+        <v-card-title class="headline">
+          Welcome to the Vuetify + Nuxt.js template
+        </v-card-title>
+        <v-card-text>
+          <p>
+            Thank you for developing with Vuetify and I look forward to bringing
+            more exciting features in the future.
+          </p>
+          <div class="text-xs-right">
+            <em><small>&mdash; John Leider</small></em>
+          </div>
+          <hr class="my-3" />
+          <a href="https://nuxtjs.org/" target="_blank">
+            Nuxt Documentation
+          </a>
+          <br />
+          <a href="https://github.com/nuxt/nuxt.js" target="_blank">
+            Nuxt GitHub
+          </a>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="primary" nuxt to="/inspire">
+            Continue
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {}
+</script>
